b9f62c2ee235dfed6a98841f8a84e99b9d44e2e5
Djod1 <daooddzodi@gmail.com>
2022-01-25 19:48:22 +0300

    aboba

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='+', intents=discord.Intents.all())
 
+id=os.environ["CHANNEL"]
+
 
 @bot.event
 async def on_member_join(member):
